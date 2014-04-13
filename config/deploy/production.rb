698c6216b2f5405472e4587c1ac09388b4a94284
Ben Dixon <ben@hillsbede.co.uk>
2014-04-13 19:46:52 +0100

    user consistent example hostnames

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
 set :stage, :production
 set :branch, "master"
-set :server_name, "rdratestbuild.talkingquickly.co.uk"
+set :server_name, "www.example.com example.com"
 
 # used in case we're deploying multiple versions of the same
 # app side by side. Also provides quick sanity checks when looking
 # at filepaths
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
-server 'hostname.tld', user: 'deploy', roles: %w{web app db}, primary: true
+server 'example.com', user: 'deploy', roles: %w{web app db}, primary: true
 
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
