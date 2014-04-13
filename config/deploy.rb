16c7ae351c1f2b0482dbde3a3d17ec31067bca4a
Ben Dixon <ben@hillsbede.co.uk>
2014-04-13 19:46:33 +0100

    by default don't symlink application.yml or run specs automatically

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,21 +15,20 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :keep_releases, 5
 
 # files we want symlinking to specific entries in shared
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml}
 
 # dirs we want symlinking to shared
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # what specs should be run before deployment is allowed to
 # continue, see lib/capistrano/tasks/run_tests.cap
-set :tests, ["spec"]
+set :tests, []
 
 # which config files should be copied by deploy:setup_config
 # see documentation in lib/capistrano/tasks/setup_config.cap
 # for details of operations
 set(:config_files, %w(
   nginx.conf
-  application.yml
   database.example.yml
   log_rotation
   monit
